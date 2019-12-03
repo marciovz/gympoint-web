18a5d9230114b7b40724eec0244eb80d1dd84099
marciovz <marciovz@yahoo.com.br>
2019-12-03 17:26:39 -0300

    Configuração inicial das rotas do projeto

---
+++ src/pages/Help_order/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Help_order() {
+  return <h1>Pedidos</h1>;
+}
---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import Plan from '../pages/Plan';
+import Student from '../pages/Student';
+import Enrollment from '../pages/Enrollment';
+import Help_order from '../pages/Help_order';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/plan" component={Plan} />
+      <Route path="/student" component={Student} />
+      <Route path="/enrollment" component={Enrollment} />
+      <Route path="/help_order" component={Help_order} />
+    </Switch>
+  );
+}
